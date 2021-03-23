6b7bbd04a65f7323c6241feee3049b958d6c9517
Vestriger <gerashchenko.p@mail.ru>
2021-03-23 17:04:31 +0300

    last commit

--- Vestriger/lab2_2.2.6.c
+++ Vestriger/lab2_2.2.6.c
@@ -5,10 +5,10 @@ int n = 0;
 float x, eps;
 
 double sinRecursion(double);
+double sinIter();
 
 int main()
 {
-    n = 0;
     printf("Enter X: ");
     scanf_s("%f", &x);
     printf("Enter eps: ");
@@ -16,7 +16,10 @@ int main()
 
     printf("Math sin(x): %f\n", sin(x));
     printf("Recursion sin(x):  %f\n", sinRecursion(x));
-    printf("Number of stacks: %d\n", n);
+    printf("Number of stacks: %d\n", ++n);
+
+    printf("Iteration sin(x): %f\n", sinIter());
+    printf("Number of iterations: %d\n", n);
 
     return 0;
 }
@@ -29,4 +32,21 @@ double sinRecursion(double p)
         n++;
         return p + sinRecursion(-p * x * x / (2 * n) / (2 * n + 1));
     }
+}
+
+
+double sinIter()
+{
+    double p, sum;
+    sum = p = x;
+    
+    n = 1;
+    while (fabs(p) > eps)
+    {
+        p *= -x*x / (2 * n) / (2 * n + 1);
+        sum += p;
+        n++;
+    }
+    return sum;
+
 }
