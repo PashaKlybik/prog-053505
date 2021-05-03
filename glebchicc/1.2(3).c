a10e7e6988cbf3cc2126a93ea4fcb2efa48dbab7
glebchicc <glebbytkevich@tut.by>
2021-05-04 00:38:19 +0300

    Буткевич Глеб lab#1 variant-#3
    Определить k-ю цифру последовательности
    182764125216343… ,
    в которой выписаны подряд кубы натуральных чисел.

--- glebchicc/1.2(3).c
+++ glebchicc/1.2(3).c
@@ -1,32 +1,46 @@
-#include <stdio.h>;
-#include <math.h>;
+#include <stdio.h>
+#include <math.h>
+
+#pragma warning(disable : 4996)
+
+int PowInt(int x, int n)
+{
+	int p = 1;
+	for (int i = 0; i < n; i++) p *= x;
+	return p;
+}
 
 void main() {
 	int k, temp = 0;
 	int count = 1, i = 2;
+
 	
 	printf("Enter the index: ");
-	scanf_s("%d", &k);
-	while (k <= 0) {
-		printf("Incorrect input, try again: ");
-		scanf_s("%d", &k);
+	for (int i = 0; i < 1; i++) { //проверка ввода
+		char eol = 0;
+		if (scanf("%d%c", &k, &eol) != 2 || eol != '\n' || k < 0) {
+			printf("Incorrect input, try again: \n");
+			scanf("%*[^\n]");
+			--i;
+		}
 	}
 	
 	while (count < k) {
-		temp = pow(i, 3);
+		temp = (int)pow(i, 3);
 		i++;
-		count += ceil(log10(temp));
+		count += (int)ceil(log10(temp));
 	}
 
 	if (count > k) {
-		temp /= 10 * (count - k);
+		temp /= (int)pow(10, abs(count - k));
 	}
 
-	int j = ceil(log10(temp));
+	int j = (int)ceil(log10(temp));
 	if (j != 1) {
-		temp %= (10 * (j - 1));
+		for (int i = 0; i < abs(j - 1); i++) {
+			temp %= 10;
+		}
 	}
 
-	printf("Result: ");
-	printf("%d", temp);
+	printf("Result: %d", temp);
 }
