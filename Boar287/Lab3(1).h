3d996dec42cab1a24739b2efdd1de81c282f50d4
Boar287 <79213467+Boar287@users.noreply.github.com>
2021-05-28 18:58:18 +0300

    Lab3

---
+++ Boar287/Lab3(1).h
@@ -0,0 +1,6 @@
+#pragma once
+#include <math.h>
+#include <stdio.h>
+int GaussMethod(float** arr, int ROWS, int COLS, float EPS);
+void  PrintMatrix(float** arr, int ROWS, int COLS);
+void FillMatrix(float** arr, int ROWS, int COLS);
