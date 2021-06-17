6230f14c13de264da36c5909a4f054d653045ab2
glebchicc <glebbytkevich@tut.by>
2021-06-18 02:01:52 +0300

    fix lab2

--- glebchicc/2.2.6.c
+++ glebchicc/2.2.6.c
@@ -1,41 +1,59 @@
 #include <stdio.h>
 #include <math.h>
 
-double x, e;
-int n = 0;
+double fact(int n) {
+    if (n < 0)
+        return 0;
 
-double Recursive(double p) {
-    if (fabs(p) < e) {
-        return p;
-    }
-    else {
-        n++;
-        return p + Recursive(-p * x * x / (2 * n) / (2 * n + 1));
-    }
+    if (n == 0)
+        return 1;
+
+    else
+        return n * fact(n - 1);
 }
 
-double Iterative() {
-    double v, sum;
-    sum = v = x;
-    n = 1;
-    while (fabs(v) > e) {
-        v *= -x * x / (2 * n) / (2 * n + 1);
-        sum += v;
-        n++;
+double recursion(double x, double eps, int n) {
+    double recs = (pow(-1, (double)n - 1) * pow(x, (double)2 * n - 1) / fact(2 * n - 1));
+
+    if (fabs(recs) <= eps)
+    {
+        n--;
+        return 0;
     }
+    n++;
+    return (recs + recursion(x, eps, n));
+}
+
+double iterative(double x) {
+    double n = x;
+    double sum = 0.0;
+    int counter = 1;
+
+    do
+    {
+        sum += n;
+        n *= -1.0 * x * x / ((2 * (double)counter) * (2 * (double)counter + 1));
+        counter++;
+    } while ((n > 0.0 ? n : -n) > 1E-12);
+
     return sum;
 }
 
 int main()
 {
-    printf("Enter x: ");
+    double x, sum = 0.0, epsylon;
+    int i = 1;
+    printf("¬ведите x: ");
     scanf_s("%lf", &x);
-    printf("Enter e: ");
-    scanf_s("%lf", &e);
+    printf("¬ведите epsylon: ");
+    scanf_s("%lf", &epsylon);
 
-    printf("Recursive: %.10lf\n", Recursive(x));
-    printf("n1: %d\n", ++n);
-    printf("Iterative: %.10lf\n", Iterative());
-    printf("n2: %d\n", n);
-    printf("Sinus: %.10lf\n", sin(x));
+    while (x > 6.28f) {
+        x -= 6.28f;
+    }
+    
+    printf("Recursion = %f\n", recursion(x, epsylon, 1));
+    printf("Iterative = %f\n", iterative(x));
+    printf("Sinus = %f\n", sin(x));
+    return 0;
 }
