6f9bb8a0f62b98b2ab8880d900cc36281a77759e
OriginalFlavour <79210874+OriginalFlavour@users.noreply.github.com>
2021-06-18 23:02:29 +0300

    Панкратов Кирилл lab1 variant-6

---
+++ OriginalFlavour/lab1.c
@@ -0,0 +1,24 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+    char text[100];
+    int result;
+    printf("Enter decimal number : ");
+    gets_s(text, 100);
+    for (int i = 0; text[i] != '\0'; i++) {
+        if (text[i] == ',') {
+            if (text[i + 1] && text[i + 2] && text[i + 3]) {
+                result = (text[i + 1] - '0') + (text[i + 2] - '0') + (text[i + 3] - '0');
+                printf("%d", result);
+            }
+            else {
+                printf("Error.Missing digits after ','");
+            }
+        }
+    }
+    return 0;
+}
