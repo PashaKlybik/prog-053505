83f5b524538387cc214ad5614fecab2dd08bc318
Denfit9 <79207007+Denfit9@users.noreply.github.com>
2021-06-21 19:42:40 +0300

    Карачун Денис Lab#6 variant-#10 (#120)
    * Create 1
    
    * Delete 1
    
    * Create 1
    
    * F
    
    * Update 5.2.10.c

---
+++ Denfit9/5.2.10.c
@@ -0,0 +1,106 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define Size 100
+typedef struct tnode {
+    char* word;
+    struct tnode* left;
+    struct tnode* right;
+}Tnode;
+
+char* doublestr(const char* str) {                 //клонирование строки
+    char* d = malloc(strlen(str) + 1);   
+    if (d == NULL) 
+    {
+        return NULL;
+    }
+    strcpy(d, str);                        
+    return d;                           
+}
+
+void treeprint(Tnode* tr, int level) {           //вывод дерева, хотя по условию,вроде, этого не требуется
+
+    if (tr != NULL) 
+    {
+        treeprint(tr->left, level + 1);
+        for (int i = 0; i <= level; i++) 
+            printf("   ");
+        printf("%s\n", tr->word);
+        treeprint(tr->right, level + 1);
+
+    }
+}
+
+ Tnode* addtree(struct tnode* tr, char* w) {       //добавление элемента дерева
+    if (tr == NULL) 
+    {
+        tr = (Tnode*)malloc(sizeof(Tnode));
+        tr->word = doublestr(w);
+        tr->left = tr->right = NULL;
+    }
+    int compare;
+    compare = strcmp(w, tr->word);
+    if (compare < 0)
+    {
+        tr->left = addtree(tr->left, w);
+    }
+    else if (compare > 0)
+    {
+        tr->right = addtree(tr->right, w);
+    }
+    return tr;
+}
+
+void deletetree(Tnode* tree) {                       //очистка дерева
+    if (tree != NULL) 
+    {
+        deletetree(tree->left);
+        deletetree(tree->right);
+        free(tree->word);
+        free(tree);
+    }
+}
+int rightword(char* str)                       //проверка является ли слово палиндромом
+{
+    int lenght = strlen(str);
+    for (int i = 0; i < lenght / 2; i++)
+    {
+        if (str[i] != str[lenght - 1 - i])
+            return 0;
+    }
+    return 1;
+}
+
+void palindrome(Tnode* tr) {                    //поиск палиндромов в дереве
+
+    if (tr != NULL) 
+    {
+        palindrome(tr->left);
+        if (rightword(tr->word))
+            printf("%s\n", tr->word);
+        palindrome(tr->right);
+    }
+}
+
+int main() {
+    Tnode* root=NULL;
+    char newWord[Size];
+    printf("Enter your words, ""end"" will be your last word\n");
+    do 
+    {
+        scanf_s("%s", newWord, Size);
+        if (isalpha(newWord[0]) && strcmp(newWord, "end") != 0)
+            root = addtree(root, newWord);
+    } while (strcmp(newWord, "end") != 0);   
+    printf("\n");
+    printf("Binary Tree:\n");
+    treeprint(root, 0);
+    printf("\n");
+    printf("Palindromes in an alphabetical order:\n");
+    palindrome(root);
+    deletetree(root);
+    return 0;
+}
