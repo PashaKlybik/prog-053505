5ac98a25a12518e584fdad925eab38a127628979
nikita122002 <73549310+nikita122002@users.noreply.github.com>
2021-06-21 22:37:40 +0300

    Харлап Никита lab#2.2 var#6 (#101)
    * Харлап Никита lab#2.2 var#6
    
    * Update lab2.2.6.cpp
    
    * Update lab2.2.6.cpp

---
+++ nikita122002/lab2.2.6.cpp
@@ -0,0 +1,77 @@
+﻿/*lab2.2 var#6 
+ Численно убедиться в справедливости равенства, для чего для
+заданного значения х вычислить его левую часть и разложение,
+стоящее в правой части. При каком n исследуемое выражение
+отличается от sin x менее, чем на заданную погрешность ε?
+Испытать разложение на сходимость при разных значениях х.*/
+#include <stdio.h>
+#include <math.h>
+
+int Factorial(int n)
+{
+    if (n == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return n * Factorial(n - 1);
+    }
+}
+
+void Iteration(float x, float epsilon)
+{
+    int count = 0;
+    float sinus = 0, math = sinf(x);
+
+    do
+    {
+        count++;
+        sinus += pow(-1, count - 1) * pow(x, 2 * count - 1) / Factorial(2 * count - 1);
+    } while (math - epsilon > sinus || math + epsilon < sinus);
+
+    printf_s("Math sin(x): %lf\nsin(x) using iterations = %lf\nn2:%d\n", math, sinus,count);
+}
+
+float Recursion(float x, float eps, int* n)
+{
+    float recs = (pow(-1, *n - 1) * pow(x, 2 * (*n) - 1) / Factorial(2 * (*n) - 1));
+    if (fabs(recs) <= eps)
+    {
+        (*n)--;
+        return 0;
+    }
+    (*n)++;
+    return (recs + Recursion(x, eps, n));
+}
+
+int main()
+{
+    float epsilon, radian;
+    int x, period, cycles = 1;
+
+    printf_s("Enter X:");
+    do {
+        res = scanf_s("%d", &(x));
+        while (getchar() != '\n');
+        if (res == 1) {}
+        else printf("%s", "Bad,you can enter only numbers.Repeat enter:\n");
+    } while (res != 1);
+    if (x > 180)
+    {
+        period = x / 180;
+        x -= 180 * period;
+    }
+    printf_s("Enter epsilon: ");
+    do {
+        res1 = scanf_s("%d", &(epsilon));
+        while (getchar() != '\n');
+        if (res1 == 1) {}
+        else printf("%s", "Bad,you can enter only numbers.Repeat enter:\n");
+    } while (res1 != 1);
+    radian = (float)x / 180 * 3.14;
+    printf_s("sin(x) using recursion = %lf\n", Recursion(radian, epsilon, &cycles));
+    printf_s("n1: %d\n\n", cycles);
+    Iteration(radian, epsilon);
+    return 0;
+}
