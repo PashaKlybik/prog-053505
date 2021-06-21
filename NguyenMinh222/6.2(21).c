92c9ea50238a20b5f57539481e18ae30e690fc1e
NguyenMinh222 <79207246+NguyenMinh222@users.noreply.github.com>
2021-06-22 00:18:54 +0300

    Нгуен Ба Куанг Минь lab#7var#21(без разговоров)) (#123)

---
+++ NguyenMinh222/6.2(21).c
@@ -0,0 +1,35 @@
+#include "MyLibrary.h"
+
+int main()
+{
+    file_name = "lyrics.txt";   //отсюда считываем и записываем сюда же
+
+    get_data_from_file();
+
+    main_menu();
+
+    write_data_to_file();
+
+#pragma region clearing heap
+
+    int i;
+
+    for (i = 0; i < num_of_articles; i++)
+        delete_article(articles[i]);
+    free(articles);
+
+    for (i = 0; i < num_of_groups; i++)
+        delete_group(groups[i]);
+    free(groups);
+
+    for (i = 0; i < num_of_releases; i++)
+        delete_release(releases[i]);
+    free(releases);
+
+    for (i = 0; i < num_of_users; i++)
+        delete_user(users[i]);
+    free(users);
+
+#pragma endregion
+
+}
